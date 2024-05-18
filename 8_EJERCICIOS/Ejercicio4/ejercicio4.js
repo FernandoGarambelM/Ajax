eae8e817a92aa9cce282b17d6c035750490ec990
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-05-17 21:51:41 -0500

    Ejercicio 4: Funcion mostrarGrafico terminada, ahora ya se muestra el grafico en la pagina

--- 8_EJERCICIOS/Ejercicio4/ejercicio4.js
+++ 8_EJERCICIOS/Ejercicio4/ejercicio4.js
@@ -1,17 +1,42 @@
 document.addEventListener("DOMContentLoaded", function(){
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "data.json". true);
+    xhr.open("GET", "../data.json", true);
     xhr.onload = function() {
-        if (xhr.status = 200) {
+        if (xhr.status === 200) {
             let data = JSON.parse(xhr.responseText);
             let arequipaData = data.find(region => region.region === "Arequipa");
             if (arequipaData) {
-                mostrarGrafico(arequipaData);
+                mostrarGrafico(arequipaData.confirmed);
+            } else {
+                console.error("No se econtraron datos de Arequipa");
             }
+        } else {
+            console.error('Error en la red:',xhr.statusText);
         }
-    }
+    };
+    xhr.onerror = function() {
+        console.error('Error en la red');
+    };
+    xhr.send();
 })
 
-function mostrarGrafico(data) {
+function mostrarGrafico(arequipaData) {
+    let dates = arequipaData.map(entry => entry.date);
+    let values = arequipaData.map(entry => parseInt(entry.value));
     
+    let ctx = document.getElementById('Grafico').getContext('2d');
+    let graphic = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: 'Numero de Confirmados',
+                data: values,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                fill: false,
+            }]
+        }
+        
+    });
 }
